b02a6f43b263b33d2e250deae816692acce08d47
David <davohak2009@gmail.com>
2026-09-25 23:05:51 +0300

    feat: update next.config.ts to include output and image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   transpilePackages: ["@splidejs/react-splide"],
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
